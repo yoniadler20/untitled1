d7ca25e99a0e0d7c0b57d92085f3c09e4f1375a4
Eliyahu Tronto <a3938081@gmail.com>
2022-05-03 21:39:42 +0300

    youni

--- src/main.java
+++ src/main.java
@@ -1,7 +1,9 @@
 public class main {
     public static void main(String[] args) {
-        int x =3;
-        int y =3;
-        int t =3;
+
+        String s ="eli";
+    }
+    public void m(){
+        System.out.println("youn");
     }
 }
